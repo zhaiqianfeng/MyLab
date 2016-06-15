9842446b1ab0b2ceddf6564bc19f0705e08ae577
zhaiqianfeng <zhaiqianfeng@163.com>
2016-06-15 12:55:57 +0800

    modify

--- src/main/java/com/zhaiqianfeng/Lab/JavaBean/LabApp.java
+++ src/main/java/com/zhaiqianfeng/Lab/JavaBean/LabApp.java
@@ -1,3 +1,11 @@
+/**
+ * 测试javabean的概念
+ * JavaBean特征：
+ * 		有public无参构造器
+ * 		属性private，通过public方法取赋值，比如set\get
+ * 		基于事件思想的触发，多用于swing
+ * 有java.beans包的支持
+ */
 package com.zhaiqianfeng.Lab.JavaBean;
 
 public class LabApp {
--- src/main/java/com/zhaiqianfeng/Lab/JavaBean/MyBean.java
+++ src/main/java/com/zhaiqianfeng/Lab/JavaBean/MyBean.java
@@ -4,15 +4,21 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
+/**
+ * 观察者模式
+ * 把观察者注册到PropertyChangeSupport中，观察者需要实现PropertyChangeListener接口
+ * 通过firePropertyChange把事件通知观察者
+ *
+ */
 public class MyBean implements PropertyChangeListener {
 	private int userId;
 	private String userName;
-	private PropertyChangeSupport pcs=new PropertyChangeSupport(this);
-	
-	public MyBean(){
+	private PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+
+	public MyBean() {
 		pcs.addPropertyChangeListener(this);
 	}
-	
+
 	public int getUserId() {
 		return userId;
 	}
@@ -26,13 +32,13 @@ public class MyBean implements PropertyChangeListener {
 	}
 
 	public void setUserName(String userName) {
-		String oldName=this.userName;
+		String oldName = this.userName;
 		this.userName = userName;
 		pcs.firePropertyChange("name", oldName, userName);
 	}
 
 	@Override
 	public void propertyChange(PropertyChangeEvent evt) {
-		System.out.println(String.format("%s,%s,%s", evt.getPropertyName(),evt.getOldValue(),evt.getNewValue()));
+		System.out.println(String.format("%s,%s,%s", evt.getPropertyName(), evt.getOldValue(), evt.getNewValue()));
 	}
 }
